e8659a30f28a012e9495ebda2257defc5d6343f7
Fajar Alfath <mfalfath25@gmail.com>
2025-12-07 10:18:30 +0700

    fix: clock

--- src/components/section/experiment/blocks/Clock.tsx
+++ src/components/section/experiment/blocks/Clock.tsx
@@ -4,37 +4,47 @@ import { useState, useEffect, useRef } from 'react'
 import { DateTime } from 'luxon'
 
 export const Clock = () => {
-  const [currentTime, setCurrentTime] = useState<string>(
-    DateTime.now().toLocaleString(DateTime.TIME_SIMPLE)
-  )
-  const [currentDate, setCurrentDate] = useState<string>(
-    DateTime.now().toLocaleString({
-      weekday: 'short',
-      month: 'short',
-      day: '2-digit',
-    })
-  )
+  const [currentTime, setCurrentTime] = useState<string | null>(null)
+  const [currentDate, setCurrentDate] = useState<string | null>(null)
 
-  const intervalRef = useRef<NodeJS.Timeout>()
+  const intervalRef = useRef<NodeJS.Timeout | null>(null)
 
   useEffect(() => {
-    intervalRef.current = setInterval(() => {
+    const updateTime = () => {
       const now = DateTime.now()
-      setCurrentTime(DateTime.now().toLocaleString(DateTime.TIME_SIMPLE))
+      setCurrentTime(now.toLocaleString(DateTime.TIME_SIMPLE))
       setCurrentDate(
-        now.toLocaleString({ weekday: 'short', month: 'short', day: '2-digit' })
+        now.toLocaleString({
+          weekday: 'short',
+          month: 'short',
+          day: '2-digit',
+        })
       )
-    }, 1000)
+    }
+
+    updateTime() // Initial call
+
+    intervalRef.current = setInterval(updateTime, 1000)
 
     return () => {
-      clearInterval(intervalRef.current)
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current)
+      }
     }
   }, [])
+
+  if (!currentTime || !currentDate) {
+    return (
+      <div className='mx-auto flex h-full items-center justify-center'>
+        <div className='shadow-accent-light dark:shadow-accent-dark flex h-24 w-32 animate-pulse flex-col items-center justify-center rounded-2xl border-4 border-double border-neutral-600 shadow-md' />
+      </div>
+    )
+  }
 
   return (
     <>
       <div className='mx-auto flex h-full items-center justify-center'>
-        <div className='flex h-24 w-32 flex-col items-center justify-center rounded-2xl border-4 border-double border-neutral-600 shadow-md shadow-accent-light dark:shadow-accent-dark'>
+        <div className='shadow-accent-light dark:shadow-accent-dark flex h-24 w-32 flex-col items-center justify-center rounded-2xl border-4 border-double border-neutral-600 shadow-md'>
           <p className='animate-pulse-fast text-lg font-semibold'>
             {currentTime}
           </p>
